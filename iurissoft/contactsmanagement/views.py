813cc57af3b566ef78de4b0a83f0e2ce6a1ef97e
Gilwell Muhati <gilwellm@gmail.com>
2018-09-22 00:32:58 +0300

    Created the Contacts Management View and Urls and Linkened to the index url

--- iurissoft/contactsmanagement/views.py
+++ iurissoft/contactsmanagement/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def contact_list(request):
+    '''context = {
+        "items": items,
+        "page_range": page_range
+    }'''
+
+    template = "contactsmanagement/contacts_list.html"
+
+    return render(request, template)
